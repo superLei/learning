c51a1d4d4400eefd31ba34854be1ee114a6a6d13
su <su@sudeiMac.local>
2020-09-22 23:42:27 +0800

    add

---
+++ src/main/java/learn/ReplaceSpace.java
@@ -0,0 +1,21 @@
+package learn;
+
+/**
+ * 请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为 "We Are Happy". 则经过替换之后的字符串为 "We%20Are%20Happy"。
+ *
+ * */
+public class ReplaceSpace {
+
+    public static String replaceSpace(StringBuffer str){
+       if(str.length() == 0) return "";
+        String s= str.toString();
+        int j =0;
+        for (int i = 0; i < s.length(); i++) {
+           if(s.charAt(i) == ' '){
+               int index =
+               str.replace()
+           }
+        }
+    }
+
+}
