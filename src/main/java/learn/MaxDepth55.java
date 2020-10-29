f46921adfea48f6ec3b22bcd55b4f77d7a53a3b1
su <su@sudeiMac.local>
2020-10-29 23:26:00 +0800

    55

---
+++ src/main/java/learn/MaxDepth55.java
@@ -0,0 +1,45 @@
+package learn;
+
+
+import sun.tools.tree.Node;
+
+import java.util.TreeMap;
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ */
+public class MaxDepth55 {
+    /**
+     * 树结构定义
+     *
+     * */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     *
+     * */
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+    }
+}
