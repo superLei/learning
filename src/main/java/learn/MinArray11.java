0610774898875a24192a73ee14bd221b0c86f3c7
su <su@MacBook-Pro.local>
2020-11-03 17:26:54 +0800

    03

--- src/main/java/learn/MinArray11.java
+++ src/main/java/learn/MinArray11.java
@@ -1,5 +1,7 @@
 package learn;
 
+import java.util.Optional;
+
 /**
  * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组
  * [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
@@ -80,11 +82,25 @@ public class MinArray11 {
         }
         return numbers[start];
     }
+
+    private static class A{
+        private String cardid;
+
+        private A(String cardid) {
+            this.cardid = cardid;
+        }
+
+        public void setCardid(String cardid) {
+            this.cardid = cardid;
+        }
+    }
     public static void main(String[] args) {
 //        int[] nums = {7,8,9,4,5};
 //        int[] nums = {2,2,2,0,1};
         int[] nums = {6,7,1,2,3,4,5};
         System.out.println(minArray(nums));
         System.out.println(minArray3(nums));
+        A a = null;
+        Optional.ofNullable(a).ifPresent(x -> x.setCardid("123"));
     }
 }
